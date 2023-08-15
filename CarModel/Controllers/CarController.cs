56aa176fc24c5b494da63c7d5bbc85cab56f19b2
Alin <Alin@DESKTOP-ME2M9DN>
2023-08-15 14:48:26 +0300

    Add project files.

---
+++ CarModel/Controllers/CarController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarModel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private List<string> models = new List<string> { "Dacia", "Volkswagen", "Fiat", "Renault" };
+        [HttpGet]
+
+        public ActionResult GetModels()
+        {
+ 
+            if (models.Count == 0)
+                return NotFound();
+            return Ok(models);
+        }
+        [HttpPost]
+        public ActionResult CreatNewModels(string newModel)
+        {
+            if (string.IsNullOrWhiteSpace(newModel))
+                return BadRequest("Invalid model name");
+            models.Add(newModel);
+            return CreatedAtAction(nameof(GetModels), models);
+        }
+
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteModels(string id)
+        {
+            bool badThingsHappened = false;
+
+            if (badThingsHappened)
+                return BadRequest();
+            return NoContent();
+        }
+    }
+}
